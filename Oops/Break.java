72c02bb9c99c9cc3e8effea47f49ee9baf73cf03
deepshikharay2000 <76932499+deepshikharay2000@users.noreply.github.com>
2021-03-14 20:20:51 +0530

    Add files via upload

---
+++ Oops/Break.java
@@ -0,0 +1,26 @@
+package Oops;
+
+public class Break {
+    public static void main(String[] args) {
+            
+    
+    
+        int i;
+        for(i=0;i <=10;i++) {
+             if(i == 5) {
+                break;
+            }
+            System.out.println("i="+ i);
+        
+        }
+
+        
+    }
+}  
+
+
+
+
+
+    
+
